a08a717c57fc06b4a8b30838cac556c31cb3e8ac
HassnainKazmi <hassnainkazmi07@gmail.com>
2022-12-01 02:45:11 +0500

    Superscript functionality implemented

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const xpower = document.querySelector(".xp");
 const xunder = document.querySelector(".xu");
 const togglePadding = document.querySelector(".togglePadding");
 
-const keys = [" ", "!", ",", "(", ")", "%", "CE", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const keys = [" ", "^", "!", ",", "(", ")", "%", "CE", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let result = "";
 let newResult = "";
 let isInputPressed = false;
